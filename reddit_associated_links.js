aef54357cc8d60c6682df3f6f6b3126d732710fc
nsfzebras <nsfzebras@gmail.com>
2014-08-26 22:25:27 -0400

    jQueryUI modal working. css importing.

--- reddit_associated_links.js
+++ reddit_associated_links.js
@@ -14,6 +14,8 @@ javascript:(function() {
                 2:  Put results in a jQueryUI modal instead of a simple alert box.
    */
 
+  var globalVersion = "v1.0.0 - NSF_Zebras" ;
+
   function main() { 
 
     importJQUI() ; 
@@ -25,10 +27,76 @@ javascript:(function() {
 
   }
 
+  function run( selectedLink ) { 
+
+    jQuery.ajax({
+
+      url: "http://www.reddit.com/submit?url=" + jQuery( selectedLink ).attr( "href" ),
+      type: "GET"
+
+    }).success( function( seenit ) { 
+
+      searchCrossPosts( seenit ) ; 
+
+    }) ; 
+
+  }
+
+  function searchCrossPosts( seenit ) { 
+
+    var links = new Array() ;
+    var otherSubmissionsCount = jQuery( "a.comments", seenit ).length ; 
+
+    jQuery( "a.comments", seenit ).each( function( key, commentsUrl ) { 
+
+      /*console.debug( key + " -> " + value ) ; */
+      searchSpecificPost( links, commentsUrl ) ; 
+
+    }) ; 
+
+    links.sort( function (a,b) {
+      return b.split( "|" )[0] - a.split( "|" )[0] ;
+    }) ; 
+
+    showModal( links ) ; 
+
+  }
+
+  function searchSpecificPost( links, commentsUrl ) { 
+
+    jQuery.ajax({
+
+      url: commentsUrl,
+      type: "GET",
+      async: false
+
+    }).success( function( data ){
+
+      jQuery( "div.entry", data ).each( function( key, comment ) { 
+
+        var points = jQuery( "span.unvoted", comment ).text().split( " " )[ 0 ] ; 
+        var link = jQuery( "div.usertext-body p a", comment ) ; 
+
+        if( link ) { 
+
+          var joined = points + "|" + jQuery( link ).attr( "href" ) + "|" + jQuery( link ).text() ; 
+          /*console.log( joined ) ; */
+
+          links.push( joined ) ;
+
+        }
+
+      })  ; 
+
+    }) ;
+
+  }
+
   function importJQUI(){
 
     if( !window.jQuery.ui ) {
       
+      /*import jQueryUI js*/
       var fileref = document.createElement( 'script' ) ;
 
       fileref.setAttribute( "type", "text/javascript" ) ;
@@ -36,7 +104,48 @@ javascript:(function() {
 
       document.getElementsByTagName( "head" )[0].appendChild( fileref ) ;
 
+      /*import jQueryUI css (smoothness theme)*/
+      var cssref = document.createElement( 'link' ) ;
+
+      cssref.setAttribute( "rel", "stylesheet" ) ;
+      cssref.setAttribute( "type", "text/css" ) ; 
+      cssref.setAttribute( "href", "//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css" ) ;
+
+      document.getElementsByTagName( "head" )[0].appendChild( cssref ) ;
+
     }    
+
+  }
+
+  function showModal( links ) {
+    
+    /*First remove all previously added modals*/
+    jQuery( "div#dialog" ).remove() ;
+
+    var div = buildDiv( links ) ; 
+    jQuery( "body" ).append( div ) ; 
+    jQuery( "div#dialog" ).dialog({
+      width:'auto'
+    }) ;
+
+  }
+
+  function buildDiv( links ) { 
+
+    var div = "<div id='dialog' title='R.A.L. - " + globalVersion + "'>" ;
+
+    for( var i = 0 ; i < links.length ; i++ ) { 
+
+      var linkSplit = links[i].split( "|" ) ; 
+      if( linkSplit[1] != "undefined" ) { 
+        div += "<p>" + linkSplit[0] + " -> <a href='" + linkSplit[1] + "'>" + linkSplit[2] + "</a></p>" ; 
+      }
+
+    }
+
+    div += "</div>" ;
+
+    return div ; 
 
   }
 
@@ -50,68 +159,6 @@ javascript:(function() {
     }
     
     return $('a').filter(function(index) { return $(this).text() === text ; }) ;
-
-  }
-
-  function run( selectedLink ) { 
-
-    jQuery.ajax({
-
-      url: "http://www.reddit.com/submit?url=" + jQuery( selectedLink ).attr( "href" ),
-      type: "GET"
-
-    }).success( function( seenit ) { 
-
-      var links = new Array() ;
-      var otherSubmissionsCount = jQuery( "a.comments", seenit ).length ; 
-
-      jQuery( "a.comments", seenit ).each( function( key, value ) { 
-
-        /*console.debug( key + " -> " + value ) ; */
-        jQuery.ajax({
-
-          url: value,
-          type: "GET",
-          async: false
-
-        }).success( function( data ){
-
-          jQuery( "div.entry", data ).each( function( key, value ) { 
-
-            var points = jQuery( "span.unvoted", value ).text().split( " " )[ 0 ] ; 
-            var link = jQuery( "div.usertext-body p a", value ).attr( "href" ) ; 
-
-            if( link ) { 
-
-              var joined = points + " -> " + link ; 
-              /*console.log( joined ) ; */
-
-              links.push( joined ) ;
-
-            }
-
-          })  ; 
-
-        }) ;
-
-      }) ; 
-
-      links.sort( function (a,b) {
-        return b.split( " " )[0] - a.split( " " )[0] ;
-      }) ; 
-
-      var alertMessage = "Highlighted link found in " + otherSubmissionsCount + " submissions. Below is every link submitted as a comment on those submissions.  \n\nPoints -> Link.\n" ; 
-      for( var i = 0 ; i < links.length ; i++ ) { 
-        alertMessage += links[i] + "\n" ;
-      }
-
-      if( links.length ) { 
-        alert( alertMessage ) ; 
-      } else { 
-        alert( "No links found." ) ;
-      }
-
-    }) ; 
 
   }
 
