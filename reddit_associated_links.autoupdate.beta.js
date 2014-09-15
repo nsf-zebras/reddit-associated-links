ef080227656d6bb3047267e8d68498a4b28149cc
nsfzebras <nsfzebras@gmail.com>
2014-09-15 00:46:55 -0400

    Adds helper script that can automatically pull the latest content from the github project page.

---
+++ reddit_associated_links.autoupdate.beta.js
@@ -0,0 +1,32 @@
+javascript:(function() {
+
+	/*  
+      AUTHOR:       NSF_Zebras 
+
+      Version:      1.0-b1
+
+      Description:  This bookmarklet will pull the source from git each time it is run.  
+      				Therefore automatically getting the latest and greatest version each time.
+
+	  Branch:       Beta
+
+   */
+
+    /* 
+    	Change this between the various "release" branches.  Which can be found in the readme on the project page: 
+		https://github.com/nsf-zebras/reddit-associated-links
+    */
+    var ralBranch = "beta" ;
+
+	/* First remove any previous imports. */
+	jQuery( "script[src*='reddit-associated-links']" ).remove() ;
+
+	/* Then import the reddit associated links script. */
+	var fileref = document.createElement( 'script' ) ;
+
+	fileref.setAttribute( "type", "text/javascript" ) ;
+	fileref.setAttribute( "src", "//raw.githubusercontent.com/nsf-zebras/reddit-associated-links/" + ralBranch + "/reddit_associated_links.js" ) ;
+
+	document.getElementsByTagName( "head" )[0].appendChild( fileref ) ;
+
+})();
